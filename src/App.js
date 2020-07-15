65f7e2a502d34c32c5d0c522fdf8997ace572850
Isaac Cohen <issc29@gmail.com>
2020-07-15 13:10:16 -0400

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        unsafeText:''
+    };
 }
+
+ setText = () => {
+  let formText = "Predefined Text"
+
+  this.setState({ 
+    unsafeText: (formText) ? formText : "Username or Password can not be empty"
+  })
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div 
+              dangerouslySetInnerHTML={{__html: this.state.unsafeText}}>
+          </div>
+          <button onClick={this.setText} >Get URL Parameter</button>
+        </header>
+      </div>
+    );
+  }
+}
+
 
 export default App;
